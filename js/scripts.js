0dbe2608ec6d8e689540ce8247ca73fb8745d1c5
inendi8 <paul.jennings@inendi.com>
2022-10-30 18:34:55 +0000

    Back to Top button

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,29 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+//Back-To-Top button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
